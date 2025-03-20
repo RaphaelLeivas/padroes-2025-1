da490e9a8fb9c47e857b29293a6419f98b5c4e1e
RaphaelLeivas <raphaelhenrique2013@outlook.com>
2025-03-20 14:11:49 -0300

    ex1 knn finalizado

--- ex1_knn.R
+++ ex1_knn.R
@@ -5,6 +5,8 @@ if (length(dev.list())) {
 
 library("plot3D")
 
+set.seed(203)
+
 minkowski_distance <- function(p1, p2, p) {
   # p1 =  ponto 1, p2 = ponto 2
   # p = 2 é distancia euclidiana
@@ -35,7 +37,7 @@ myknn <- function(Xarg, Yarg, xt, k) {
     current_point <- matrix(X[i,], ncol = 2)
     
     # ambos pontos devem estar na forma de matriz - cuidado
-    dist_to_point <- minkowski_distance(current_point, xt, p = 4)
+    dist_to_point <- minkowski_distance(current_point, xt, p = 2)
     label_of_point <- Y[i]
     
     dist_matrix[i,] <- c(dist_to_point, label_of_point)
@@ -52,19 +54,35 @@ myknn <- function(Xarg, Yarg, xt, k) {
 }
 
 # aberturas das gaussianas: quanto maior s, mais ela espalha
-s1<-0.8
-s2<-0.8
+s1<-0.75
+s2<-0.75
 
 nc<-100 # numero de pontos de cada classe
 
-k_list <- c(3, 5, 9)
+k_list <- c(3, 5, 9, 17)
+
+par(mfrow=c(2,2), cex.lab=2, cex.axis=2, cex.main=2, mai = c(0.4, 0.4, 0.4, 0.4))
+
+# gera os dados - fora do loop, para que sejam os mesmo dados para todos os k
+# centro dessa gaussiana esta na posicao (2,2)
+xc1<-matrix(rnorm( nc*2 ) , ncol=2)*s1 + t (matrix( c ( 2 , 2 ) ,nrow=2,ncol=nc ) )
+# centro dessa outra gaussiana esta na posicao (4,4)
+xc2<-matrix(rnorm( nc*2 ) , ncol=2)*s2 + t (matrix( c ( 4 , 4 ) ,nrow=2,ncol=nc ) )
+
+# matriz de entrada
+X <- rbind(xc1, xc2)
+
+# rotulos de entrada
+yc1 <- rep(1, nc)
+yc2 <- rep(-1, nc)
+Y <- c(yc1, yc2)
+
+# gera o grid
+x1grid <- seq(0, 6, 0.1)
+x2grid <- seq(0, 6, 0.1)
+grid_matrix <- matrix(NA, nrow = length(x1grid), ncol = length(x2grid))
 
 for (k in k_list) {
-  # centro dessa gaussiana esta na posicao (2,2)
-  xc1<-matrix(rnorm( nc*2 ) , ncol=2)*s1 + t (matrix( c ( 2 , 2 ) ,nrow=2,ncol=nc ) )
-  # centro dessa outra gaussiana esta na posicao (4,4)
-  xc2<-matrix(rnorm( nc*2 ) , ncol=2)*s2 + t (matrix( c ( 4 , 4 ) ,nrow=2,ncol=nc ) )
-  
   plot(
     NULL,
     main = paste("KNN: k = ", k),
@@ -74,21 +92,8 @@ for (k in k_list) {
     xlim = c(0, 6)
   )
   
-  points(xc1, col="red")
-  points(xc2, col="blue")
-  
-  # matriz de entrada
-  X <- rbind(xc1, xc2)
-  
-  # rotulos de entrada
-  yc1 <- rep(1, nc)
-  yc2 <- rep(-1, nc)
-  Y <- c(yc1, yc2)
-  
-  # gera o grid
-  x1grid <- seq(0, 6, 0.1)
-  x2grid <- seq(0, 6, 0.1)
-  grid_matrix <- matrix(NA, nrow = length(x1grid), ncol = length(x2grid))
+  points(xc1, col="red", lwd=2)
+  points(xc2, col="blue", lwd=2)
   
   for (i in 1:length(x1grid)) {
     for (j in 1:length(x2grid)) {
