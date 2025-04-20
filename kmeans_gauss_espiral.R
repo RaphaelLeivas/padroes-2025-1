8f63485b9ebc1b4254d5d4a9ab421a8df0fa68e8
RaphaelLeivas <raphaelhenrique2013@outlook.com>
2025-04-19 22:23:53 -0300

    classificacao inicial exercicio 1

--- kmeans_gauss_espiral.R
+++ kmeans_gauss_espiral.R
@@ -7,9 +7,9 @@ library("plot3D")
 library("mlbench")
 source("C:\\dev\\padroes-2025-1\\utils\\mistura_gaussiana.R")
 
-xlim <- c(-2, 2)
-ylim <- c(-2, 2)
-grid_seq <- seq(-2, 2, 0.05)
+xlim <- c(-1.7, 1.7)
+ylim <- c(-1.7, 1.7)
+grid_seq <- seq(-1.7, 1.7, 0.05)
 
 N <- 1000
 k <- 30
@@ -17,17 +17,32 @@ n <- 2 # dimensao
 C1_LABEL <- 1
 C2_LABEL <- 2
 
+n_folds <- 10
+fold_size <- floor(N / n_folds)
+
+acc_array <- c()
+
 all_data <- mlbench.spirals(N, cycles=2, sd=0.04)
-plot(all_data, xlim = xlim, ylim = ylim)
+
+plot(all_data, xlim = xlim, ylim = ylim, lwd = 2)
 
 xall <- cbind(all_data$x, as.numeric(all_data$classes))
 
-xall_c1 <- xall[which(xall[,n+1]==C2_LABEL),]
-xall_c2 <- xall[which(xall[,n+1]==C1_LABEL),]
+# embaralha a matriz dos dados de entrada - remove bias de coleta
+xall <- xall[sample.int(nrow(xall)), ]
+
+n_folds <- 10
+fold_size <- floor(N / n_folds)
+
+acc_array <- c()
+
+xall_c1 <- xall[which(xall[,n+1]==C1_LABEL),]
+xall_c2 <- xall[which(xall[,n+1]==C2_LABEL),]
 
 pc1 <- nrow(xall_c1) / nrow(xall)
 pc2 <- nrow(xall_c2) / nrow(xall)
 
+# BUG()
 kmeansret_c1 <- kmeans(xall_c1, k)
 kmeansret_c2 <- kmeans(xall_c2, k)
 
@@ -70,11 +85,15 @@ for (i in seqi) {
 
 plot(xall_c1[,1], xall_c1[,2], col=kmeansret_c1$cluster, lwd = 2, xlim = xlim, ylim = ylim)
 par(new=T)
-contour(seqi, seqj, M1_c1, xlim = xlim, ylim = ylim) # mostra que é uma funcao continua, que junta as gaussianas todas
+contour(seqi, seqj, M1_c1, xlim = xlim, ylim = ylim, lwd = 2) # mostra que é uma funcao continua, que junta as gaussianas todas
+par(new=T)
+points(kmeansret_c1$centers[,1:n], col = "blue", pch = 4, lwd = 4)
 
 plot(xall_c2[,1], xall_c2[,2], col=kmeansret_c2$cluster, lwd = 2, xlim = xlim, ylim = ylim)
 par(new=T)
-contour(seqi, seqj, M1_c2, xlim = xlim, ylim = ylim) # mostra que é uma funcao continua, que junta as gaussianas todas
+contour(seqi, seqj, M1_c2, xlim = xlim, ylim = ylim,  lwd = 2) # mostra que é uma funcao continua, que junta as gaussianas todas
+par(new=T)
+points(kmeansret_c2$centers[,1:n], col = "blue", pch = 4, lwd = 4)
 
 # persp3D(seqi, seqj, M1_c1)
 # persp3D(seqi, seqj, M1_c2)
