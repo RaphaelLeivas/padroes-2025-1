4a5226427b79f2f8c2e746f43b6b48222541b724
RaphaelLeivas <raphaelhenrique2013@outlook.com>
2025-04-28 18:14:04 -0300

    espaco de verossimilhanca estavel com validacao cruzada

--- ex_espaco_verossimilhancas.R
+++ ex_espaco_verossimilhancas.R
@@ -3,7 +3,9 @@ if (length(dev.list())) {
   dev.off()
 }
 
+library("flextable")
 library("plot3D")
+
 
 fnormal1var <- function(x, m, r) {
   y <- (1/(sqrt(2*pi*r*r)))*exp(-0.5 * ((x-m)/r)^2)
@@ -14,6 +16,7 @@ set.seed(203)
 
 C1_LABEL <- 1
 C2_LABEL <- -1
+grid_spacing <- 0.1
 
 # xi é cada amostra
 kdemulti <- function(xi, xall, h) {
@@ -45,7 +48,7 @@ kdemulti <- function(xi, xall, h) {
 }
 
 
-N <- 120
+N <- 300
 n <- 2
 # h <- 0.5
 
@@ -69,70 +72,123 @@ yc2 <- rep(C2_LABEL, nrow(xc1))
 xall <- rbind(g1, g2, g3, g4)
 yall <- c(yc1, yc2)
 
-pc1 <- nrow(xc1) / nrow(xall)
-pc2 <- nrow(xc2) / nrow(xall)
+# junta tudo na matriz dos dados
+all_data <- cbind(xall, yall)
 
-h_list <- c(0.05, 0.1, 0.5, 1)
+# embaralha a matriz dos dados de entrada - remove bias de coleta
+all_data <- all_data[sample.int(nrow(all_data)), ]
+
+n_folds <- 10
+fold_size <- floor(N / n_folds)
+
+h_list <- c(0.001, 0.01, 0.1, 1, 10)
+
+acc_by_h <- c()
 
 for (h in h_list) {
-  par(mfrow=c(1,2))
   
-  plot(xc1[,1], xc1[,2], xlim = c(0,6), ylim=c(0,6), xlab='', ylab='', col = "red", , main = paste("h = ", h))
-  par(new=T)
-  plot(xc2[,1], xc2[,2], xlim = c(0,6), ylim=c(0,6), xlab='', ylab='', col = "blue")
+  acc_array <- c()
   
-  # px<-kdemulti(xall[1,], xall, h)
-  pxc1<-kdemulti(xc2[1,], xc1, h)
-  pxc2<-kdemulti(xc2[1,], xc2, h)
-  print(pxc1 / pxc2)
-  
-  # grid
-  seqi <- seq(0, 6, 0.1)
-  seqj <- seq(0, 6, 0.1)
-  M1 <- matrix(1, nrow =  length(seqi), ncol = length(seqj))
-  
-  ci <- 0
-  
-  for (i in seqi) {
-    ci <- ci + 1
-    cj <- 0
+  for (fold in 1:n_folds) {
+    num_of_corrects <- 0
     
-    for (j in seqj) {
-      cj <- cj +1
-      x <- matrix(c(i, j), byrow = T, ncol = 1)
-      pxc1 <- kdemulti(x, xc1, h)
-      pxc2 <- kdemulti(x, xc2, h)
+    start_index <- fold_size * (fold - 1) + 1
+    end_index <- start_index + fold_size
+    
+    data_for_test <- all_data[start_index:end_index, ]
+    X_test <- data_for_test[, 1:n]
+    Y_test <- data_for_test[, n+1]
+    
+    data_for_train <- all_data[-(start_index:end_index), ]
+    X_train <- data_for_train[, 1:n]
+    Y_train <- data_for_train[, n+1]
+    
+    xc1_train <- X_train[which(Y_train == C1_LABEL),]
+    xc2_train <- X_train[which(Y_train == C2_LABEL),]
+    
+    pc1 <- nrow(xc1_train) / nrow(X_train)
+    pc2 <- nrow(xc2_train) / nrow(X_train)
+    
+    # plot(xc1_train[,1], xc1_train[,2], xlim = c(0,6), ylim=c(0,6), xlab='', ylab='', col = "red", , main = paste("h = ", h))
+    # par(new=T)
+    # plot(xc2_train[,1], xc2_train[,2], xlim = c(0,6), ylim=c(0,6), xlab='', ylab='', col = "blue")
+    
+    # grid
+    seqi <- seq(0, 6, grid_spacing)
+    seqj <- seq(0, 6, grid_spacing)
+    M1 <- matrix(1, nrow =  length(seqi), ncol = length(seqj))
+    
+    ci <- 0
+    
+    for (i in seqi) {
+      ci <- ci + 1
+      cj <- 0
       
-      if (pxc1 * pc1 > pxc2 * pc2) {
-        M1[ci, cj] <- C1_LABEL
-      } else {
-        M1[ci, cj] <- C2_LABEL
+      for (j in seqj) {
+        cj <- cj +1
+        x <- matrix(c(i, j), byrow = T, ncol = 1)
+        pxc1 <- kdemulti(x, xc1_train, h)
+        pxc2 <- kdemulti(x, xc2_train, h)
+        
+        if (pxc1 * pc1 > pxc2 * pc2) {
+          M1[ci, cj] <- C1_LABEL
+        } else {
+          M1[ci, cj] <- C2_LABEL
+        }
       }
     }
+    
+    # par(new = T)
+    # contour(seqi, seqj, M1, levels = 1, lwd = 2)
+    
+    # calcula a acuracia no conjunto de testes
+    for (i in 1:nrow(data_for_test)) {
+      xt <- data_for_test[i, 1:n]
+      yt <- data_for_test[i, n+1]
+      
+      # coloca o xt no grid. ve se esta correto
+      # procura a posição do grid o mais proximo possivel do xtest_point
+      # o valor de cada posicao no xgrid é (index - 1) * grid_spacing
+      # queremos (index - 1) * grid_spacing = xtest_point => index = xtest_point / grid_spacing + 1
+      index_in_xgrid = round(xt[1] / grid_spacing + 1)
+      index_in_ygrid = round(xt[2] / grid_spacing + 1)
+      
+      if (M1[index_in_xgrid, index_in_ygrid] == yt) {
+        num_of_corrects <- num_of_corrects + 1
+      }
+    }
+    
+    acc_array <- c(acc_array, num_of_corrects / nrow(data_for_test) * 100)
+    
+    # Nall <- nrow(X_train)
+    # 
+    # pxc1vec <- matrix()
+    # pxc2vec <- matrix()
+    # 
+    # for (i in 1:Nall) {
+    #   # para amostra calcular o kde dela e guardar
+    #   pxc1vec[i] <- kdemulti(Xtrain[i,], xc1, h)
+    #   pxc2vec[i] <- kdemulti(Xtrain[i,], xc2, h)
+    # }
+    # 
+    # pxc1c2 <- cbind(pxc1vec, pxc2vec)
+    # col_seq <- c("red", "blue")
+    # 
+    # plot(pxc1c2[,1], pxc1c2[,2], col = col_seq[((yall+1)/2) + 1], 
+    #      xlab="pxc1", ylab="pxc2", main = paste("h = ", h))
   }
   
-  par(new = T)
-  contour(seqi, seqj, M1, levels = 1, lwd = 2)
-  # ribbon3D(seqi, seqj, M1)
+  acc_by_h <- c(acc_by_h, mean(acc_array))
   
-  Nall <- nrow(xall)
+  print(paste(mean(acc_array), " +/- ", sd(acc_array)))
   
-  pxc1vec <- matrix()
-  pxc2vec <- matrix()
-  yhat_tst <- c()
-  
-  for (i in 1:Nall) {
-    # para amostra calcular o kde dela e guardar
-    pxc1vec[i] <- kdemulti(xall[i,], xc1, h)
-    pxc2vec[i] <- kdemulti(xall[i,], xc2, h)
-  }
-  
-  pxc1c2 <- cbind(pxc1vec, pxc2vec)
-  col_seq <- c("red", "blue")
-  
-  plot(pxc1c2[,1], pxc1c2[,2], col = col_seq[((yall+1)/2) + 1], 
-       xlab="pxc1", ylab="pxc2", main = paste("h = ", h))
+  df <- data.frame(seq(1, 10, 1), round(acc_array, 2))
+  colnames(df) <- c("Fold", "Acurácia (%)")
+  ft <- flextable(df)
+  ft <- align(ft, align = "center", part = "all")
 }
+
+plot(h_list, acc_by_h, lwd = 2, col = "black", type = "b")
 
 
 
